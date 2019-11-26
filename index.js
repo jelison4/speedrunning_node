4498f0f28acf76e484799554f31b36c2d9ba2969
Josh Elison <joshelison@gmail.com>
2019-11-25 21:54:25 -0700

    sdf

--- index.js
+++ index.js
@@ -1,11 +1,16 @@
 const express = require('express');
 const path = require('path');
+require('dotenv').config();
 
 const table = require('./controllers/tableController');
 const game = require('./controllers/gameContoller');
 const category = require('./controllers/categoryController');
 
 const PORT = process.env.PORT || 5000;
+
+const {Pool} = require("pg");
+const connectionString = process.env.DATABASE_URL || "postgres://vxgyyvdfhqbcyu:812573ea7f869d6b1ae95d831285b305a583abd3a3d85bdea78a0d187b482d1a@ec2-174-129-255-69.compute-1.amazonaws.com:5432/d7spsmk4hhi5pk?ssl=true";
+const pool = new Pool({connectionString: connectionString});
 
 var app = express();
 
