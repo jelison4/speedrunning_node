ba19130509064b171e2e0acc583995708507f70b
Josh Elison <joshelison@gmail.com>
2019-11-25 23:16:50 -0700

    removed some of the console.logs

--- controllers/categoryController.js
+++ controllers/categoryController.js
@@ -7,15 +7,12 @@ function getCategorys(req, res) {
 
     if (id != 0) {
         getcategoryFromDB(id, function (error, result) {
-            console.log("back from database with result", result);
             res.json(result);
         });
     }
 }
 
 function getcategoryFromDB(id, callback) {
-    console.log("accessing DB for categoys for game", id);
-
     var sql = `SELECT DISTINCT run.category_id, category.category_title FROM run, category WHERE run.category_id = category.id AND run.game_id=${id};`;
 
     pool.query(sql, function (err, res) {
